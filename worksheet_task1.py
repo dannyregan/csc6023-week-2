8c81ea21dca0a866acecb0b015acc291eb3f6703
Danny Regan <d.regan@columbia.edu>
2025-02-03 13:47:04 +0100

    Creates a balanced matrix and calculates the number of possible permutations.

---
+++ worksheet_task1.py
@@ -0,0 +1,47 @@
+from itertools import combinations
+
+def permutations(n):
+    ones = list(combinations(list(range(n)),n//2))
+    ans = []
+    for o in ones:
+        case = []
+        for i in range(n):
+            if (i in o):
+                case.append(1)
+            else:
+                case.append(0)
+        ans.append(case)
+    return ans
+
+def check(mat):
+    n = len(mat[0])
+    for j in range(n):
+        acc0, acc1 = 0, 0
+        for i in range(len(mat)):
+            if (mat[i][j] == 1):
+                acc1 += 1
+            elif (mat[i][j] == 0):
+                acc0 += 1
+            if (acc0 > (n//2)) or (acc1 > n//2):
+                return False
+    return True
+
+def layer(r, mat, perm, ans):
+    for p in perm:
+        mat.append(p)
+        if check(mat):
+            if (r+1 == len(p)):
+                ans += 1
+            else:
+                ans = layer(r+1, mat, perm, ans)
+        mat.pop()
+    return ans
+
+def balanced01mat(n):
+    perm = permutations(n)
+    ans = layer(0, [], perm, 0)
+    return ans
+
+print("Matrix Order 2:", balanced01mat(2)) # 2
+print("Matrix Order 4:", balanced01mat(4)) # 90
+print("Matrix Order 6:", balanced01mat(6)) # 297200
